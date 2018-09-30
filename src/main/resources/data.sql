06609743fd378e2c030de4c1f0eabd1d05e55671
Alfred <alfredoborja81194@gmail.com>
2018-09-30 14:02:59 +0800

    added new customer 1

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,2 +1,5 @@
 insert into admin(id, authority, address, contact_no, email, enabled, full_name, password, username)
 values(1, 'ADMIN', 'Sample Address', '09996668084', 'alfredoborja81194@gmail.com', true, 'Alfred Borja', '$2a$10$IKPjhnpgg0XdPZbBnOF6cuxCzilfoNQNyBBn/jPUy/txD/5eZmOXO', 'admin1');
+
+insert into customer(id, authority, address, contact_no, email, enabled, full_name, password, username)
+values(2, 'CUSTOMER', 'Sample Address', '09996668084', 'customer1@gmail.com', true, 'Tristan Rosales', '$2a$10$IKPjhnpgg0XdPZbBnOF6cuxCzilfoNQNyBBn/jPUy/txD/5eZmOXO', 'customer1');
